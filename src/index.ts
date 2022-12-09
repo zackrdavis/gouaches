5559e7d22a4f0672ac6a11b1e7373f43f4f7e4c7
Zack Davis <zackrdavis@gmail.com>
2022-12-08 17:33:53 -0800

    map rgba colors and track selectedColor

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 const swatches = document.querySelectorAll(".swatch");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
+
 ctx.lineWidth = 1;
 ctx.strokeStyle = "black";
 
+type Coords = [number, number];
+type Color = [number, number, number, number];
+
+const colors: { [key: string]: Color } = {
+  black: [0, 0, 0, 1],
+  red: [255, 0, 0, 1],
+  green: [0, 255, 0, 1],
+  blue: [0, 0, 255, 1],
+};
+
 let drawing = true;
-let lastMouseMovePos: false | [number, number] = false;
+let lastMouseMovePos: false | Coords = false;
+let selectedColor = colors.black;
 
 /** Draw a single pixel */
-const drawColorPixel = (pos: [number, number], color: number[]) => {
+const drawColorPixel = (pos: Coords, color: Color) => {
   const [x, y] = pos;
   const [r, g, b, a] = color;
 
@@ -29,7 +41,7 @@ const drawColorPixel = (pos: [number, number], color: number[]) => {
 };
 
 /** Draw a black line between two points */
-const lineBetween = (pos1: [number, number], pos2: [number, number]) => {
+const lineBetween = (pos1: Coords, pos2: Coords) => {
   if (lastMouseMovePos) {
     const [x1, y1] = pos1;
     const [x2, y2] = pos2;
@@ -53,7 +65,7 @@ const handleMouseDown = (e: PointerEvent) => {
   lastMouseMovePos = [x, y];
 
   // draw the pixel
-  drawColorPixel([x, y], [0, 0, 0, 255]);
+  drawColorPixel([x, y], selectedColor);
 };
 
 const handleMouseMove = (e: PointerEvent) => {
@@ -75,7 +87,9 @@ const handleMouseUp = (e: PointerEvent) => {
 
 const handleSwatchClick = (e: PointerEvent) => {
   const target = e.target as HTMLDivElement;
-  console.log(target.style.background);
+  const colorName = target.style.background;
+  selectedColor = colors[colorName];
+  ctx.strokeStyle = `rgba(${selectedColor.join(",")})`;
 };
 
 document.addEventListener("mousedown", handleMouseDown);
